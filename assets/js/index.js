0f60e405e824bb0c5d0470840a9986a946457943
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2022-04-18 07:05:17 -0300

    📦 NEW: Modularization

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+// import { setup_pomodoro } from "./pomodoro_ui.js";
+import { setup_task_list } from "./task_list_ui.js";
+import { setup_date } from "./date_header_ui.js";
+
+setup_date();
+setup_task_list();
+// setup_pomodoro();
