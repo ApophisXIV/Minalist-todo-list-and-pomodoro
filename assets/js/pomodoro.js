f5a15e86a119bbfcb434b61eb1bdf4d69dd2f0ec
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2022-04-18 07:05:48 -0300

    📦 NEW: Pomodoro logic (class) and renderer

---
+++ assets/js/pomodoro.js
@@ -0,0 +1,104 @@
+// 25 + 5 + 25 + 5 + 25 + 5 + 25 + 15
+class Pomodoro {
+    constructor(pomodoro_config = { pomodoro_duration: 25, break_duration: 5, long_break_duration: 15, long_break_after: 1 }) {
+        this.pomodoro_duration = pomodoro_config.pomodoro_duration * 60
+        this.break_duration = pomodoro_config.break_duration * 60
+        this.long_break_duration = pomodoro_config.long_break_duration * 60
+        this.long_break_interval = pomodoro_config.long_break_after
+        this.update_ui_callback = pomodoro_config.update_ui_callback || function () { }
+        this.end_callback = pomodoro_config.end_callback || function () { }
+
+    }
+
+    set_pomodoro_duration = duration => { this.pomodoro_duration = duration * 60 }
+    set_break_duration = duration => { this.break_duration = duration * 60 }
+    set_long_break_duration = duration => { this.long_break_duration = duration * 60 }
+    set_long_break_interval = interval => { this.long_break_interval = interval }
+
+    get_actual_config = () => { return { pomodoro_duration: this.pomodoro_duration / 60, break_duration: this.break_duration / 60, long_break_duration: this.long_break_duration / 60, long_break_after: this.long_break_interval } }
+
+    set_update_ui_callback = callback => { this.update_ui_callback = callback }
+    set_callback_on_end = callback => { this.end_callback = callback }
+
+    set_actual_state = state => { this.actual_state = state }
+    get_actual_state = () => { return this.actual_state }
+
+    set_actual_duration = duration => { this.actual_duration = duration }
+    get_actual_duration = () => { return this.actual_duration }
+
+    get_duration_from_state = state => { return state === 'pomodoro' ? this.pomodoro_duration : state === 'break' ? this.break_duration : this.long_break_duration }
+    get_progress_percentage = () => { return 100 - (100 * this.actual_duration / this.get_duration_from_state(this.actual_state)) }
+    get_actual_time = () => {
+        return {
+            min: parseInt(this.actual_duration / 60).toString().padStart(2, '0'),
+            sec: (this.actual_duration % 60).toString().padStart(2, '0')
+        }
+    }
+
+    is_running = () => { return this.timer !== undefined }
+
+    start = () => {
+        if (this.is_running()) return // Avoid multiple timers
+
+        this.set_actual_state(this.get_actual_state() === undefined ? 'pomodoro' : this.get_actual_state())
+        this.set_actual_duration(this.get_actual_duration() === undefined ? this.pomodoro_duration : this.get_actual_duration())
+        this.actual_interval = this.long_break_interval
+
+        console.log('Config: ', this.pomodoro_duration, this.break_duration, this.long_break_duration, this.long_break_interval)
+
+        this.timer = setInterval(() => {
+
+            // console.log('Running', this.get_actual_time(), this.get_actual_state());
+            this.set_actual_duration(this.get_actual_duration() - 1)
+            this.update_ui_callback()
+
+            switch (this.actual_state) {
+                case 'pomodoro':
+                    if (this.get_actual_duration() !== 0) return
+                    this.set_actual_state('break')
+                    this.set_actual_duration(this.break_duration)
+
+                    if (this.actual_interval !== 0) return
+                    this.set_actual_state('long break')
+                    this.set_actual_duration(this.long_break_duration)
+                    this.actual_interval = this.long_break_interval
+                    break
+
+                case 'break':
+                    if (this.get_actual_duration() !== 0) return
+                    this.set_actual_state('pomodoro')
+                    this.set_actual_duration(this.pomodoro_duration)
+                    this.actual_interval--
+                    break
+
+                case 'long break':
+                    if (this.get_actual_duration() !== 0) return
+                    this.set_actual_state('pomodoro')
+                    this.reset()
+                    this.end_callback()
+                    break
+            }
+        }, 1000)
+    }
+
+    pause = () => {
+        // console.log('Pause');
+        clearInterval(this.timer)
+        this.timer = undefined
+        return { time: this.get_actual_time(), state: this.get_actual_state() }
+    }
+
+    reset = () => {
+        // console.log('Reset');
+        this.pause()
+        this.set_actual_state('pomodoro')
+        this.set_actual_duration(this.pomodoro_duration)
+        this.update_ui_callback()
+    }
+
+}
+
+// Singleton
+const pomodoro = new Pomodoro()
+export default pomodoro
+
---
+++ assets/js/pomodoro_ui.js
@@ -0,0 +1,135 @@
+import pomodoro from './pomodoro.js';
+import { select } from './utils.js';
+
+// ---- Pomodoro DOM ----
+const pomo_timer = {
+    output: select('#pomodoro-stage-timer'),
+    progress: select('#pomodoro-progress-bar'),
+};
+const pomo_controls = {
+    btn_start_pause: select('#pomodoro-start-pause-btn'),
+    btn_reset: select('#pomodoro-reset-btn'),
+};
+const pomo_settings = {
+    pomo_settings: select('#pomodoro-user-settings'),
+    work: {
+        input_range: select('#pomodoro-range-work'),
+        label: select('#pomodoro-work-time-label'),
+    },
+    break: {
+        input_range: select('#pomodoro-range-break'),
+        label: select('#pomodoro-break-time-label'),
+    },
+    long_break: {
+        input_range: select('#pomodoro-range-long-break'),
+        label: select('#pomodoro-long-break-time-label'),
+    },
+    interval: {
+        input_range: select('#pomodoro-range-intervals'),
+        label: select('#pomodoro-intervals-label'),
+    },
+    confirm: select('#config-confirm'),
+};
+
+// ---- Pomodoro UI ----
+const update_on_end = () => {
+    pomo_timer.output.innerHTML = `<i>Let's go work!</i>`;
+    pomo_controls.btn_start_pause.innerText = 'Start';
+    pomo_controls.btn_start_pause.classList.add('at-btn-outline-info');
+    pomo_controls.btn_start_pause.classList.remove('at-btn-outline-white');
+}
+
+const update_ui = () => {
+    const time = pomodoro.get_actual_time();
+    const state = pomodoro.get_actual_state();
+    const progress = pomodoro.get_progress_percentage();
+
+    pomo_timer.output.innerHTML = `<i>${state}</i> ${time.min}:${time.sec}`;
+    pomo_timer.progress.setAttribute('value', progress);
+
+    if (state === 'pomodoro') {
+        pomo_timer.progress.classList.toggle('pomodoro-progress-bar-bg', true);
+        pomo_timer.progress.classList.toggle('break-progress-bar-bg', false);
+        pomo_timer.progress.classList.toggle('long-break-progress-bar-bg', false);
+    } else if (state === 'break') {
+        pomo_timer.progress.classList.toggle('pomodoro-progress-bar-bg', false);
+        pomo_timer.progress.classList.toggle('break-progress-bar-bg', true);
+        pomo_timer.progress.classList.toggle('long-break-progress-bar-bg', false);
+    } else {
+        pomo_timer.progress.classList.toggle('pomodoro-progress-bar-bg', false);
+        pomo_timer.progress.classList.toggle('break-progress-bar-bg', false);
+        pomo_timer.progress.classList.toggle('long-break-progress-bar-bg', true);
+    }
+    // Accessibility stuff
+    pomo_timer.progress.setAttribute('aria-valuenow', `${progress}`);
+    pomo_timer.progress.setAttribute('aria-valuetext', `${time.min}:${time.sec}`);
+    pomo_timer.progress.setAttribute('aria-valuemax', `${pomodoro.get_duration_from_state(state)}`);
+};
+
+// ---- Pomodoro UI Events ----
+//Config pomodoro timer
+const config_pomodoro = (config) => {
+    pomodoro.set_pomodoro_duration(config.pomodoro_duration);
+    pomodoro.set_break_duration(config.break_duration);
+    pomodoro.set_long_break_duration(config.long_break_duration);
+    pomodoro.set_long_break_interval(config.long_break_interval);
+    pomodoro.reset()
+    pomodoro.set_actual_state('pomodoro');
+}
+
+//Update config
+pomo_settings.pomo_settings.addEventListener('submit', e => {
+    e.preventDefault()
+    const user_config = Object.fromEntries(new FormData(e.target))
+    config_pomodoro(user_config)
+
+    // Store config
+    localStorage.setItem('pomodoro_config', JSON.stringify(user_config));
+})
+
+// Settings listeners
+pomo_settings.work.input_range.addEventListener('input', e => {
+    pomo_settings.work.label.innerHTML = `${(e.target.value).toString().padStart(2, '0')}:00`;
+})
+pomo_settings.break.input_range.addEventListener('input', e => {
+    pomo_settings.break.label.innerHTML = `${(e.target.value).toString().padStart(2, '0')}:00`;
+})
+pomo_settings.long_break.input_range.addEventListener('input', e => {
+    pomo_settings.long_break.label.innerHTML = `${(e.target.value).toString().padStart(2, '0')}:00`;
+})
+pomo_settings.interval.input_range.addEventListener('input', e => {
+    pomo_settings.interval.label.innerHTML = `${e.target.value} ${e.target.value > 1 ? 'intervals' : 'interval'}`;
+})
+pomo_controls.btn_start_pause.addEventListener('click', () => {
+    if (pomodoro.is_running()) {
+        pomo_controls.btn_start_pause.textContent = 'Continue';
+        pomo_controls.btn_start_pause.classList.add('at-btn-outline-info');
+        pomo_controls.btn_start_pause.classList.remove('at-btn-outline-white');
+        pomodoro.pause()
+        return
+    }
+    pomo_controls.btn_start_pause.textContent = 'Pause';
+    pomo_controls.btn_start_pause.classList.add('at-btn-outline-white');
+    pomo_controls.btn_start_pause.classList.remove('at-btn-outline-info');
+    pomodoro.start()
+})
+pomo_controls.btn_reset.addEventListener('click', () => {
+    if (!pomodoro.is_running()) return
+    pomo_controls.btn_start_pause.textContent = 'Start';
+    pomo_controls.btn_start_pause.classList.add('at-btn-outline-info');
+    pomo_controls.btn_start_pause.classList.remove('at-btn-outline-white');
+    pomodoro.reset();
+})
+
+// Callbacks to update UI on Pomodoro events
+pomodoro.set_update_ui_callback(update_ui);
+pomodoro.set_callback_on_end(update_on_end);
+
+// const setup_pomodoro = () => {
+//     // Load config
+//     const user_config = JSON.parse(localStorage.getItem('pomodoro_config'))
+//     if (user_config) config_pomodoro(user_config)
+//     update_ui();
+// }
+
+// export { setup_pomodoro }
